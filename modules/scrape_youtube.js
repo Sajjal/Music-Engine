e410665c3f74ccbf868ff8eb4d4e7084125e8ff9
Sajjal <sajjalco@gmail.com>
2020-06-19 15:09:13 -0500

    Bug Fixes

--- modules/scrape_youtube.js
+++ modules/scrape_youtube.js
@@ -1,62 +1,57 @@
-const yts = require("scrape-youtube").default;
+const yts = require("yt-search");
 
-//Search Youtube based on keyword
 async function yt_search(keyword) {
   const videos = new Promise((resolve, reject) => {
-    yts.search(keyword).then((results) => {
+    yts(keyword, function (err, r) {
+      const videos = r.videos;
       let videoArr = [];
-      for (let i = 0; i < results.length; i++) {
-        let duration = "duration";
+      for (let i = 0; i < videos.length; i++) {
+        let seconds = "seconds";
         if (
-          duration in results[i] &&
-          results[i].duration <= 600 &&
-          results[i].duration > 200
+          seconds in videos[i] &&
+          videos[i].seconds <= 600 &&
+          videos[i].seconds > 200
         ) {
-          videoArr.push(results[i]);
+          videoArr.push(videos[i]);
         }
       }
       resolve(videoArr);
-      videoArr = [];
     });
   });
   await videos;
   return videos;
 }
 
-//Capitalize the first letter of title.
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 async function scrape_youtube(keyword) {
-  let records = 0;
-  while (records <= 0) {
+  let videos = [];
+  while (videos.length <= 0) {
     videos = await yt_search(keyword);
-    if (videos.length > 5) records++;
   }
 
   const videoArr = [];
   let videoList = {};
+  let count = 3;
 
-  //return array of 3 videos < 10 min in length
-  let count = 5;
   for (let i = 0; i < count; i++) {
-    let length = videos[i].duration;
-
     title = videos[i].title.substring(0, 24).toLowerCase();
     title = capitalizeFirstLetter(title);
-    lengthInMinutes = parseFloat(videos[i].duration / 60).toFixed(2);
 
     videoList = {
       title: title,
-      audioID: videos[i].id,
-      length: lengthInMinutes,
-      thumbnail: `https://i.ytimg.com/vi/${videos[i].id}/hqdefault.jpg`,
+      audioID: videos[i].videoId,
+      length: videos[i].timestamp,
+      thumbnail: `https://i.ytimg.com/vi/${videos[i].videoId}/hqdefault.jpg`,
     };
+
     videoArr.push(videoList);
 
     if (videoArr.length == 3) {
       videos = [];
+      console.log(videoArr);
       return videoArr;
     }
     count++;
