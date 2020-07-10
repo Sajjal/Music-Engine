76f6714ac267b14d3fe6f1fccfad8c746f660d44
Sajjal <sajjalco@gmail.com>
2020-07-10 02:56:38 -0500

    Performance Improvement

--- modules/scrape_youtube.js
+++ modules/scrape_youtube.js
@@ -1,60 +1,36 @@
-const yts = require("yt-search");
-
-async function yt_search(keyword) {
-  const videos = new Promise((resolve, reject) => {
-    yts(keyword, function (err, r) {
-      const videos = r.videos;
-      let videoArr = [];
-      for (let i = 0; i < videos.length; i++) {
-        let seconds = "seconds";
-        if (
-          seconds in videos[i] &&
-          videos[i].seconds <= 600 &&
-          videos[i].seconds > 200
-        ) {
-          videoArr.push(videos[i]);
-        }
-      }
-      resolve(videoArr);
-    });
-  });
-  await videos;
-  return videos;
-}
+const scrapeYt = require("scrape-yt");
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 async function scrape_youtube(keyword) {
-  let videos = [];
-  while (videos.length <= 0) {
-    videos = await yt_search(keyword);
-  }
-
-  const videoArr = [];
-  let videoList = {};
-  let count = 3;
-
-  for (let i = 0; i < count; i++) {
-    title = videos[i].title.substring(0, 24).toLowerCase();
-    title = capitalizeFirstLetter(title);
-
-    videoList = {
-      title: title,
-      audioID: videos[i].videoId,
-      length: videos[i].timestamp,
-      thumbnail: `https://i.ytimg.com/vi/${videos[i].videoId}/hqdefault.jpg`,
-    };
-
-    videoArr.push(videoList);
-
-    if (videoArr.length == 3) {
-      videos = [];
-      return videoArr;
+    let videos = [];
+    let videoList = {};
+    while (videos.length <= 0) {
+        videos = await scrapeYt.search(keyword);
     }
-    count++;
-  }
+    let videoArr = [];
+    for (let i = 0; i < videos.length; i++) {
+        let duration = "duration";
+        if (
+            duration in videos[i] &&
+            videos[i].duration <= 600 &&
+            videos[i].duration > 200
+        ) {
+            title = videos[i].title.substring(0, 24).toLowerCase();
+            title = capitalizeFirstLetter(title);
+            length = parseFloat(videos[i].duration / 60).toFixed(2)
+            videoList = {
+                title: title,
+                audioID: videos[i].id,
+                length: length,
+                thumbnail: `https://i.ytimg.com/vi/${videos[i].id}/hqdefault.jpg`,
+            };
+            videoArr.push(videoList)
+            if (videoArr.length == 3) return videoArr;
+        }
+    }
 }
 
-module.exports = { scrape_youtube };
+module.exports = {scrape_youtube};
